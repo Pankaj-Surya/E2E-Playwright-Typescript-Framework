46784cd102b9c48d65a4c824921483eb0e2563b5
Rinaldo23 <rinaldobadigar@gmail.com>
2025-06-30 21:07:51 +0530

    Add LoginPage and login test

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,8 +30,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     // trace: 'on-first-retry',
-    headless : false
+    headless: false,
   },
+
+  timeout: 60000,
 
   /* Configure projects for major browsers */
   projects: [
